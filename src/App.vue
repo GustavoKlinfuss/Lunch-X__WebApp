41e7a6319fedb618692aeb0478f5de8fb0497e84
Gustavo Klinfuss <g.kss@hotmail.com>
2020-09-30 15:32:58 -0300

    Changes in css and include of UserInfoScreenComponent

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <RequestsScreen/>
+  <div id="app" >
+    <div class="container">
+      <h1 class="header">{{header}}</h1>
+      <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+      <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+      <RequestsScreen v-if="stage === 'RequestsScreen'"/>
+      <UserInfoScreen v-if="stage === 'UserInfoScreen'"/>
+    </div>
   </div>
 </template>
 
 <script>
 import RequestsScreen from './components/RequestsScreen.vue'
-  // import HelloWorld from './components/HelloWorld.vue'
+import UserInfoScreen from './components/UserInfoScreen.vue'
 
-  export default {
-    name: 'App',
-    components: {
-      RequestsScreen,
-      // HelloWorld
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      stage: 'RequestsScreen',
+      header: 'Tempeadori - Faça seu pedido',
     }
+  },
+  components: {
+    RequestsScreen,
+    UserInfoScreen
   }
+}
 </script>
+
+<style>
+html, #app {
+  background-color: rgb(223, 223, 223);
+}
+
+.container {
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	display: flex; /* Torna o elemento um flex container automaticamente transformando todos os seus filhos diretos em flex itens. */
+	flex-direction: column; /* Faz com que os elementos sejam colocados todos em uma coluna, indo de cima para baixo */
+  font-size: 20px;
+  color:rgb(109, 109, 109);
+}
+
+.header {
+	margin-top: 8px;
+	padding: 12px;
+	background-color: rgb(235, 235, 235);
+	border: 1px solid rgb(0, 50, 107);
+}
+
+li {
+  list-style-type: none;
+}
+</style>
