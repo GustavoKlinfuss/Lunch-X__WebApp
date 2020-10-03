76997a8637a786e147fca89141e4e73af8f9e56e
Gustavo Klinfuss <g.kss@hotmail.com>
2020-10-03 17:37:36 -0300

    Attempt to create an abstraction of the b-input

---
+++ src/components/GtInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <b-form-group :label="label" label-for="inp">
+    <b-form-input id="inp" v-model="control" :placeholder="placeholder" trim></b-form-input>
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    placeholder: String,
+    controlName: String,
+    form: Object,
+    control: String
+  },
+  created: function () {
+    console.log(this.controlName);
+
+    const path = 'form.' + this.controlName;
+
+      console.log({path});
+  },
+  methods: {
+    getCoisa: function () {
+      const path = 'form.' + this.controlName;
+
+      console.log({path});
+
+      return path;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form-control::-webkit-input-placeholder {
+  color: #ccc;
+}
+</style>
