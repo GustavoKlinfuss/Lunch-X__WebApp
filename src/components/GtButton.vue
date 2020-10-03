b05940b8120635edbb73e6f135c35cde3f9b8b34
Gustavo Klinfuss <g.kss@hotmail.com>
2020-10-03 17:39:20 -0300

    Create a default button to this app

---
+++ src/components/GtButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-button size="lg" class="gt-btn" v-on:click="click()">{{text}}</b-button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  },
+  methods: {
+    click: function () {
+      this.$emit('click');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.gt-btn {
+	margin: 4px 0px 4px 0px;
+	border-radius: 0px;
+	background-color:  rgb(0, 50, 107);
+  border: 1px solid white;
+}
+</style>
