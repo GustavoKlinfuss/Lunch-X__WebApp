ba38afeb2008d12a0a0e3a35d649791276a41917
Gustavo Klinfuss <g.kss@hotmail.com>
2020-10-11 21:32:59 -0300

    Repairing bug in sunday

--- src/variables/enums.js
+++ src/variables/enums.js
@@ -10,10 +10,11 @@ export const StagesEnum = Object.freeze({
 });
 
 export const RestaurantMenu = Object.freeze({
+  0: ["Domingo não tem pedido 01", "Domingo não tem pedido 02", "Domingo não tem pedido 03"],
   1: ["Frango", "Bife"],
   2: ["Costela", "Pernil"],
   3: ["Almondega", "Coxa de Frango"],
   4: ["Frango ao molho branco", "Bife a milanesa"],
   5: ["Peixe", "Frango xadrez"],
-  6: ["Feijoada", "Strogonoff de Frango"],
+  6: ["Feijoada", "Strogonoff de Frango"]
 })
